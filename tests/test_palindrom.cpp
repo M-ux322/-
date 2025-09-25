63a5621a22829649d0aaba25907e38457cb77357
epepei <manya.ermakova@gmail.com>
2025-09-25 11:53:59 +0300

    Update test_palindrom.cpp

--- tests/test_palindrom.cpp
+++ tests/test_palindrom.cpp
@@ -2,19 +2,23 @@
 #include "../is_palindrom.h"
 
 TEST(PalindromTest, BasicPalindroms) {
-    EXPECT_TRUE(is_palindrom(121));
-    EXPECT_TRUE(is_palindrom(12321));
-    EXPECT_TRUE(is_palindrom(1));
-    EXPECT_TRUE(is_palindrom(0));
+    EXPECT_TRUE(is_palindrom("121"));
+    EXPECT_TRUE(is_palindrom("12321"));
+    EXPECT_TRUE(is_palindrom("1"));
+    EXPECT_TRUE(is_palindrom("0"));
+    EXPECT_TRUE(is_palindrom("racecar"));
+    EXPECT_TRUE(is_palindrom("a"));
 }
 
 TEST(PalindromTest, BasicNonPalindroms) {
-    EXPECT_FALSE(is_palindrom(123));
-    EXPECT_FALSE(is_palindrom(1234));
-    EXPECT_FALSE(is_palindrom(10));
+    EXPECT_FALSE(is_palindrom("123"));
+    EXPECT_FALSE(is_palindrom("1234"));
+    EXPECT_FALSE(is_palindrom("10"));
+    EXPECT_FALSE(is_palindrom("hello"));
 }
 
-TEST(PalindromTest, NegativeNumbers) {
-    EXPECT_FALSE(is_palindrom(-121));
-    EXPECT_FALSE(is_palindrom(-1));
+TEST(PalindromTest, EdgeCases) {
+    EXPECT_TRUE(is_palindrom(""));
+    EXPECT_TRUE(is_palindrom("a"));
+    EXPECT_TRUE(is_palindrom("aa"));
 }
